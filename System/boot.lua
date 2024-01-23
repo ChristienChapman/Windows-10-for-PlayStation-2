8a9f444773c873a51ad6148537279b57f599cafb
ChristienChapman <140925766+ChristienChapman@users.noreply.github.com>
2024-01-22 19:05:15 -0600

    Add files via upload

---
+++ System/boot.lua
@@ -0,0 +1,11 @@
+-- All glory to God, Jesus Christ, and the Holy Spirit.
+
+-- Put the boot for your operating system of choice
+
+dofile("System/Windows10/windows10.lua")
+
+Font.fmLoad()
+--System.initKeyboard()
+
+
+
